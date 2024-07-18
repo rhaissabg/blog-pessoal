ec34794e4f81565224221b09c5a5023f1bcae1fb
Rhaíssa Lima <rhaissasbgl@gmail.com>
2024-07-18 11:55:04 -0300

    testes com JUnit

--- src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.generation.blogpessoal.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,48 +22,57 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class BasicSecurityConfig {
-	
-	@Autowired
-	private JwtAuthFilter authFilter;
-	
-	@Bean
-	UserDetailsService userDetailsService() {
-		return new UserDetailsServiceImpl();
-	}
-	
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
-	AuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setUserDetailsService(userDetailsService());
-		authenticationProvider.setPasswordEncoder(passwordEncoder());
-		return authenticationProvider;
-	}
-	
-	@Bean
-	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-			return authenticationConfiguration.getAuthenticationManager();
-	}
-	
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.sessionManagement(management -> management
-											.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-											.csrf(csrf -> csrf.disable())
-											.cors(withDefaults());
-		http.authorizeHttpRequests((auth) -> auth
-											.requestMatchers("/usuarios/logar").permitAll()
-											.requestMatchers("/usuarios/cadastrar").permitAll()
-											.requestMatchers("/error/**").permitAll()
-											.requestMatchers(HttpMethod.OPTIONS).permitAll()
-											.anyRequest().authenticated())
-											.authenticationProvider(authenticationProvider())
-											.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-											.httpBasic(withDefaults());
+
+    @Autowired
+    private JwtAuthFilter authFilter;
+
+    @Bean
+    UserDetailsService userDetailsService() {
+
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    	http
+	        .sessionManagement(management -> management
+	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	        		.csrf(csrf -> csrf.disable())
+	        		.cors(withDefaults());
+
+    	http
+	        .authorizeHttpRequests((auth) -> auth
+	                .requestMatchers("/usuarios/logar").permitAll()
+	                .requestMatchers("/usuarios/cadastrar").permitAll()
+	                .requestMatchers("/error/**").permitAll()
+	                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+	                .anyRequest().authenticated())
+	        .authenticationProvider(authenticationProvider())
+	        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+	        .httpBasic(withDefaults());
+
 		return http.build();
-	}
-	
+
+    }
+
 }
